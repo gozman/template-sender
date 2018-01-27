965f6a61b293ce1378789f227a97795a7fdd1b66
Mike Gozzo <mike@smooch.io>
2018-01-27 22:27:22 +0100

    updating initial commit

--- handler.js
+++ handler.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const SMOOCH_ACCOUNT_KEY = "";
-const SMOOCH_ACCOUNT_SECRET = "";
-const SMOOCH_WEBHOOK_SECRET = "";
+const SMOOCH_APP_KEY = "";
+const SMOOCH_APP_SECRET = "";
 const TRIGGER_STRING = "";
 const SmoochCore = require('smooch-core');
 
@@ -10,16 +9,63 @@ module.exports.smoochWebhookListener = (event, context, callback) => {
   const headers = event.headers;
   const payload = JSON.parse(event.body);
 
+  var smooch = new SmoochCore({
+      keyId: SMOOCH_APP_KEY,
+      secret: SMOOCH_APP_SECRET,
+      scope: 'app'
+  });
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
   };
 
-  callback(null, response);
+  //Validate the trigger message
+  if(payload && payload.trigger == "message:appMaker" && payload.messages && payload.messages[0].text == TRIGGER_STRING) {
+    //Send the response
+    smooch.appUsers.sendMessage(payload.app._id, payload.appUser._id, {
+        role: 'appMaker',
+        type: 'carousel',
+        items: [{
+            title: 'Tacos',
+            description: 'These are tacos',
+            mediaUrl: 'http://example.org/image.jpg',
+            actions: [{
+                text: 'Select',
+                type: 'postback',
+                payload: 'TACOS'
+            }, {
+                text: 'More info',
+                type: 'link',
+                uri: 'http://example.org'
+            }]
+        }, {
+            title: 'Burritos',
+            description: 'Burritos are usually better than tacos',
+            mediaUrl: 'http://example.org/image.jpg',
+            actions: [{
+                text: 'Select',
+                type: 'postback',
+                payload: 'BURRITOS'
+            }, {
+                text: 'More info',
+                type: 'link',
+                uri: 'http://example.org'
+            }]
+        }]
+    }).then(() => {
+        callback(null, response);
+    });
+  } else if(payload && payload.trigger == "postback") {
+    smooch.appUsers.sendMessage(payload.app._id, payload.appUser._id, {
+        text: payload.postbacks[0].action.payload
+        role: 'appUser',
+        type: 'text'
+    }).then(() => {
+        // async code
+    });
+  } else {
+    callback(null, response);
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+
 };
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Smooch Template Sender
+
+A `serverless` function that listens for appMaker and postback webhooks from Smooch in order to fire off templated structured messages and handle postback responses.
